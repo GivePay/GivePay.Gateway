ad621c54dea89527aa8cef5449e0d347c0ee9dd2
William Ward <wward@givepaycommerce.com>
2019-03-07 11:06:11 -0600

    add AVS/CVV response codes to API response models

--- GivePay.Gateway/Transactions/Models/SaleResponse.cs
+++ GivePay.Gateway/Transactions/Models/SaleResponse.cs
@@ -10,5 +10,17 @@ namespace GivePay.Gateway.Transactions
         /// </summary>
         [DataMember(Name = "auth_code")]
         public string AuthCode { get; set; }
+
+        /// <summary>
+        /// AVS Result
+        /// </summary>
+        [DataMember(Name = "avs_result")]
+        public string AvsResult { get; set; }
+
+        /// <summary>
+        /// CVV/CVV2 Result
+        /// </summary>
+        [DataMember(Name = "cvv_result")]
+        public string CvvResult { get; set; }
     }
 }
